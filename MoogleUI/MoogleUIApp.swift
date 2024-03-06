60569700fca86a2052d3d8011ce6faf8c1e630f3
Abby Dominguez <abbytorade@icloud.com>
2024-03-06 20:17:50 +0100

    Initial Commit

---
+++ MoogleUI/MoogleUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  MoogleUIApp.swift
+//  MoogleUI
+//
+//  Created by Abby Dominguez on 6/3/24.
+//
+
+import SwiftUI
+
+@main
+struct MoogleUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
